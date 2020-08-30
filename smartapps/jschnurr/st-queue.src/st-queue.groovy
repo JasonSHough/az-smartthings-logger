8a73164c92146eba9f31b62bd8b1c57645fa9b16
Jeff Schnurr <jschnurr@gmail.com>
2020-08-30 18:43:05 -0400

    remove comment

--- smartapps/jschnurr/st-queue.src/st-queue.groovy
+++ smartapps/jschnurr/st-queue.src/st-queue.groovy
@@ -12,7 +12,6 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  */
-// import groovy.json.JsonBuilder
 
 definition(
     name: 'ST-Queue',
